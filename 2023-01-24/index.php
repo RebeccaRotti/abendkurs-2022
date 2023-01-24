1c1ca7a0f1f97fd4db5417f862651a8d153683a1
Rebecca Rottensteiner <office@rebecca-rottensteiner.at>
2023-01-24 20:44:12 +0100

    Update

--- 2023-01-24/index.php
+++ 2023-01-24/index.php
@@ -109,6 +109,110 @@
     echo $ausgabe . '<br>';
     ?>
 </div>
+<div>
+    <p>Mehrfachverzweigung mit Match</p>
+    <?php
+    echo match($wuerfel) {
+        1 => "Eins",
+        2 => "Zwei",
+        3 => "Drei",
+        4, 5, 6 => "Der Rest",
+        default => "Kein Würfelwert"
+    };
+    ?>
+</div>
+<div>
+    <p>Ternärer Operator ?: </p>
+    <?php
+        $x = 8;
+        $y = 12;
+
+        $z = $x < $y ? $x : $y;
+        echo $z . '<br>';
+
+        $f = $x < $y ? '1' : ($x > $y ? "2" : "3");
+        echo $f;
+    ?>
+</div>
+<div>
+    <p>Spaceship Operator <=></p>
+    <?php
+    echo "Wert eins: " . (12 <=> 5) . '<br>';
+    echo "Zweiter Wert: " . (5 <=> 12). '<br>';
+    echo "Gleiche Werte: " . (5 <=> 5);
+    ?>
+</div>
+<div>
+    <p>Existenz von Variablen</p>
+    <?php
+        if(isset($u)) echo $x . '<br>';
+        else echo "nicht vorhanden<br>";
+        $u = 42;
+        if(isset($u)) echo $x . '<br>';
+        else echo "nicht vorhanden<br>";
+        unset($u);
+        if(isset($u)) echo $x . '<br>';
+        else echo "nicht vorhanden<br>";
+    ?>
+</div>
+
+<div>
+    <p>Koaleszenzoperator</p>
+    <?php
+    echo $temperatur ?? "Temperatur nicht vorhanden<br>";
+    $temperatur = 36.5 . ' Celsius';
+    echo $temperatur ?? "Temp nicht vorhanden";
+    ?>
+</div>
+
+<div>
+    <p>Arrays</p>
+    <?php
+        $tp = array(14.3, 19.3, 21.3, 4.3);
+        $tp2 = array('Montag' => 14.3, 'Dienstag' => 19.3,
+            'Mittwoch' => 21.3);
+
+        //echo $tp[2] . '<br>';
+        for ($i = 0; $i < count($tp); $i++) {
+            echo $tp[$i] . '<br>';
+        }
+        foreach($tp2 as $key => $value) {
+            echo $key . ' ' .$value . '<br>';
+        }
+        foreach($tp as $value) {
+            echo $value . '<br>';
+        }
+    ?>
+</div>
+<div>
+    <p>Funktionen</p>
+    <?php
+    function add($z1, $z2) {
+        global $temperatur;
+        echo $temperatur . '<br>';
+        $summe = $z1 + $z2;
+        return $summe;
+    }
+    $c = add(3, 4);
+    echo "summe: " . $c;
+    //  echo $z1; Nicht erreichbar außerhalb der Funktion
+
+    $c = add($wuerfel, 4);
+    echo $c . '<br>';
+    ?>
+</div>
+<div>
+    <p>Continue</p>
+    <?php
+        for ($i = 1; $i <= 15; $i++) {
+            if($i >= 5 && $i <= 12) {
+                continue;
+            }
+            echo "Zeile " . $i . '<br>';
+        }
+    ?>
+
+</div>
 <!--</code>-->
 </body>
 </html>
