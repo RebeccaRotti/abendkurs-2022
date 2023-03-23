7044cffdeedc078efb1ab4c741a652ed06b3f1d9
Rebecca Rottensteiner <rebecca@creative-web.work>
2023-03-23 20:19:05 +0100

    AJAX & Fetch Example

---
+++ 2023-03-23/main.js
@@ -0,0 +1,69 @@
+const API_KEY = "59cdfcc9";
+
+function displayMovies(movies) {
+    console.log(movies);
+    let output = '';
+    movies.forEach(function(movie) {
+       output += `
+       <section>
+        <img src="${movie.Poster}" class="imgFluid">
+        <div><h2>${movie.Title}</h2>
+        <p>${movie.Year}<br>
+        ${movie.Type}</p></div></section>`;
+    });
+    $('#movies').html(output);
+}
+
+function displayPagination(page, totalPages, searchTerm) {
+    let prev = 1;
+    if(page > 1) prev = page;
+    $('#pagination').html(`<a onclick="getMovies('${searchTerm}'. ${prev+1})">Previous</a>
+                        <span>Total: ${totalPages}</span>
+                        <a onclick="getMovies('${searchTerm}', ${page+1})">Next</a>`);
+}
+
+$('#search-form').on('submit', function (event) {
+    event.preventDefault();
+    const searchTerm = $('#search').val();
+    getMovies(searchTerm, 1);
+});
+
+function getMovies(searchTerm, page) {
+    const url = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchTerm}&page=${page}`;
+
+    // AJAX
+    /*$.ajax({
+        url: url,
+        dataType: "json"
+    }).then(function (response) {
+        const movies = response.Search || [];
+        const totalPages = Math.ceil(response.totalResults / 10) || 0;
+        displayMovies(movies);
+        displayPagination(page, totalPages, searchTerm);
+    }).catch(function (error) {
+        console.error(error);
+    });*/
+
+
+    // Fetch
+    fetch(url)
+        .then(response => response.json())
+        .then(data => data.Search || [])
+        .then(function (response) {
+            const totalPages = Math.ceil(response.totalResults / 10) || 0;
+            displayMovies(response);
+            displayPagination(page, totalPages, searchTerm);
+        })
+        .catch(error => console.error(error));
+}
+
+
+
+
+
+
+
+
+
+
+
